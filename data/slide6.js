d5635a01d509a03cdb24c24349e7832f7c92f10f
Pavlo Pravdyvtsev <54671242+pravdyvtsev@users.noreply.github.com>
2024-04-05 21:50:42 +0300

    ok

--- data/slide6.js
+++
@@ -1,1 +0,0 @@
-(function(){var loadHandler=window['sl_{E3ECBF92-D231-46AF-A612-5783B52432B3}'];loadHandler&&loadHandler(5, '<div id="spr0_3691af4"><div id="spr1_3691af4" class="kern slide"><img id="img0_3691af4" src="data/img0.png" width="1280px" height="720px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_3691af4" class="kern slide"></div></div>', '{"s":[]}');})();
